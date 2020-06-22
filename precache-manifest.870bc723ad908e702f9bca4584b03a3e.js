aa45dc5f819afb92a493bfcaa539573a5b77a533
Adam Fields <adam.elliot.fields@gmail.com>
2020-06-22 02:42:14 +0000

    Updates

---
+++ precache-manifest.870bc723ad908e702f9bca4584b03a3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9098a6e9e2384512ef6dd09be1c1661b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c3436435e875384430a",
+    "url": "/static/css/main.e94bdec6.chunk.css"
+  },
+  {
+    "revision": "b782a915cb3e710b7165",
+    "url": "/static/js/2.19c052f1.chunk.js"
+  },
+  {
+    "revision": "86aa7f0f7e01374156f976952c2d860a",
+    "url": "/static/js/2.19c052f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c3436435e875384430a",
+    "url": "/static/js/main.ea89d687.chunk.js"
+  },
+  {
+    "revision": "c41fd1a39378d6a0d4f0",
+    "url": "/static/js/runtime-main.5be2fbd0.js"
+  }
+]);
